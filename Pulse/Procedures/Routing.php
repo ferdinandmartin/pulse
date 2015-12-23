f28664b48c5c5ab47718f7de0e98d146543bb93e
F.Martin <ferranleal@gmail.com>
2015-12-23 18:12:39 +0100

    Create Routing.php

---
+++ Pulse/Procedures/Routing.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+ * CONTROLLERS LOADER
+ * List of API controlllers
+ * WEB & REST
+ */
+
+require_once 'Controller/Default.php';
+
+//Add your own Controllers here:
+//require_once 'Controller/MyControler.php
+
+$API->processor();
