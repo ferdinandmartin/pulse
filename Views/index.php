cc69bf9657710419c86b9556a11d5af9003c73eb
F.Martin <ferranleal@gmail.com>
2015-12-24 13:00:41 +0100

    Update index.php

--- Views/index.php
+++ Views/index.php
@@ -17,7 +17,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="<?=basePath()?>">PULSE Framework</a>
+              <a class="navbar-brand" href="<?=basePath()?>"><img src="<?=asset('pulseLogo.png')?>" width="25"> PULSE Framework</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
               <ul class="nav navbar-nav">
